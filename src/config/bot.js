34b237f67c2c3658e5e2bc2136d23ec53567dc0c
max1822555-ui <max1822555@gmail.com>
2026-05-03 19:44:31 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Проверяет [EAC]{CHEATER} На использование читов ❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
